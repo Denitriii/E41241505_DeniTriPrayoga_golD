3dd9362e5ef8538f5d33483e914aae23aaf26a98
Denitriii <deniexriot33@gmail.com>
2025-02-07 20:09:06 +0700

    Create GithHub_Dekstop.java

---
+++ minggu 1/GithHub_Dekstop.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package githhub_dekstop;
+
+/**
+ *
+ * @author user
+ */
+public class GithHub_Dekstop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello word!");
+    }
+    
+}
